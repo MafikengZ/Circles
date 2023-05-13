f7fce2491a649e92254c41755600cdd87a3a9d4c
Sello Sydney Mafikeng <tebogomafikeng@gmail.com>
2023-05-13 09:57:59 +0200

    added exceptions , utility func

--- main.py
+++ main.py
@@ -2,20 +2,34 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 import uvicorn
 
-from models.users import User
+from models.users import *
+from utility.exceptions import *
+
 
 app =FastAPI()
 
-class UserRegistration(BaseModel):
-    user : User
-    
-    
-    @app()
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    '''Create a timedelta with the expiration time of the token.
+        Create a real JWT access token and return it'''
+        
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise 
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+
 
 if __name__ == "__main__":
-    uvicorn.run(app:'main',
-        host:str="127.0.0.1" ,
-        port: int=8080 ,
-        reload:bool=False,
-        log_level:str="debug", debug:bool=True)
-    
+    # uvicorn.run(app:'main',
+    #     host:str="127.0.0.1" ,
+    #     port: int=8080 ,
+    #     reload:bool=False,
+    #     log_level:str="debug", debug:bool=True)
+    pass
--- models/users.py
+++ models/users.py
@@ -1,14 +1,23 @@
 import uuid
 
 from pydantic import BaseModel
+from typing import Optional , Annotated, Required
+
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 from datetime import datetime, timedelta
+
+from utility.exceptions import *
+
 
 
 UUID = uuid
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 # to get a string like this run:
 # openssl rand -hex 32
@@ -42,13 +51,14 @@ class Token(BaseModel):
     access_token: str
     token_type: str
     
+class TokenData(BaseModel):
+    username: str | None = None
 
 async def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
     
-
 #Hash and verify the passwords
 async def get_password_hash(password:str):
     '''Create a utility function to hash a password coming from the user'''
@@ -58,7 +68,7 @@ async def verify_password(password:str , hash_passport:str):
     '''Verify if a received password matches the hash stored.'''
     return pwd_context.verify(password , hash_passport)
 
-
+#Get user and authenticate
 async def authenticate_user(db , username:str , password:str):
     '''authenticate and return a user.'''
     user = get_user(db , username=username)
@@ -78,3 +88,22 @@ async def create_access_token(data: dict, expires_delta: timedelta | None = None
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    '''Update get_current_user to receive the same token as before, but this time,using JWT tokens.
+        Decode the received token, verify it, and return the current user.
+        If the token is invalid, return an HTTP error right away.'''
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
